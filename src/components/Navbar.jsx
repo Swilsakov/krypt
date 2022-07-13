8794b076753e885fa68ed94861ff4d982ac0467d
djangogirls <shkodaonebob@gmail.com>
2022-07-13 17:33:30 +0600

    link to my folio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
+        <a href="https://swilsakov.github.io" className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]" target="_blank">
+          Swilsakov
+        </a>
       </ul>
       <div className="flex relative">
         {!toggleMenu && (
